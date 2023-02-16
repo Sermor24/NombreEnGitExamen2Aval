5866201bbc8e1d21e1726c4708aa6c3c068551a1
Sealmo <sealmo@floridauniversitaria.es>
2023-02-16 09:15:35 +0100

    commit

--- atotal.sh
+++ atotal.sh
@@ -8,3 +8,6 @@ while [ $(grep -c "^$ciudad " consumos.txt) -eq 0 ]; do
     echo "Introduzca el nombre de la ciudad:"
     read ciudad
 done
+
+total=$(grep "^$ciudad " consumos.txt | awk '{total += $3} END {print total}')
+echo "El total de consumo de $ciudad es: $total kWh"
